d898a254976f3d5592da1fb8a350addd275df599
hmdyt <konnyaku108@gmail.com>
2023-06-06 22:30:27 +0900

    tokenizer

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,47 @@
 pub mod token {
     use std::iter::Peekable;
 
-    fn is_digit<T: Iterator<Item = char>>(iter: &mut Peekable<T>) -> Option<bool>{
+    #[derive(Debug, PartialEq)]
+    pub enum Token {
+        Num(u32),
+        Plus,
+        Minus,
+    }
+
+    pub fn tokenize<T: Iterator<Item = char>>(iter: &mut Peekable<T>) -> Vec<Token> {
+        let mut tokens = Vec::new();
+
+        while let Some(c) = iter.peek() {
+            if c.is_whitespace() {
+                iter.next();
+                continue;
+            }
+
+            if c.is_digit(10) {
+                tokens.push(Token::Num(str_to_u(iter).unwrap()));
+                continue;
+            }
+
+            match iter.next() {
+                Some('+') => tokens.push(Token::Plus),
+                Some('-') => tokens.push(Token::Minus),
+                Some(other) => panic!("予期しない文字です: {}", other),
+                None => break,
+            }
+        }
+
+        tokens
+    }
+
+    pub fn expect_number(tokens: &mut dyn Iterator<Item = &Token>) -> Option<u32> {
+        if let Some(Token::Num(n)) = tokens.next() {
+            Some(*n)
+        } else {
+            None
+        }
+    }
+
+    fn is_digit<T: Iterator<Item = char>>(iter: &mut Peekable<T>) -> Option<bool> {
         if let Some(i) = iter.peek() {
             Some(i.is_digit(10))
         } else {
@@ -9,7 +49,7 @@ pub mod token {
         }
     }
 
-    pub fn str_to_u<T: Iterator<Item = char>>(iter: &mut Peekable<T>) -> Option<u32> {
+    fn str_to_u<T: Iterator<Item = char>>(iter: &mut Peekable<T>) -> Option<u32> {
         // 最初の文字が数字でなければNoneを返す
         if !is_digit(iter)? {
             return None;
@@ -44,6 +84,42 @@ pub mod token {
 
             let mut c = "a12".chars().peekable();
             assert_eq!(str_to_u(&mut c), None);
+        }
+
+        #[test]
+        fn test_tokenize() {
+            let mut c = "1+2".chars().peekable();
+            assert_eq!(
+                tokenize(&mut c),
+                vec![Token::Num(1), Token::Plus, Token::Num(2)]
+            );
+
+            let mut c = "1 + 2".chars().peekable();
+            assert_eq!(
+                tokenize(&mut c),
+                vec![Token::Num(1), Token::Plus, Token::Num(2)]
+            );
+
+            let mut c = "1 + 2 - 3".chars().peekable();
+            assert_eq!(
+                tokenize(&mut c),
+                vec![
+                    Token::Num(1),
+                    Token::Plus,
+                    Token::Num(2),
+                    Token::Minus,
+                    Token::Num(3)
+                ]
+            );
+        }
+
+        #[test]
+        fn test_expect_number() {
+            let tokens = vec![Token::Num(1), Token::Plus, Token::Num(2)];
+            let mut token_iter = tokens.iter();
+
+            assert_eq!(expect_number(&mut token_iter), Some(1_u32));
+            assert_eq!(expect_number(&mut token_iter), None);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,33 @@
 use std::{env, io::Result};
 
-use rust9cc::util::str_to_u;
+use rust9cc::token::{expect_number, tokenize, Token};
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    
+
     if args.len() != 2 {
         panic!("引数の数が正しくありません");
     }
     let mut c = args[1].chars().peekable();
+    let tokens = tokenize(&mut c);
+    let mut token_iter = tokens.iter().peekable();
 
+    // アセンブリの前半部分を出力
     println!(".intel_syntax noprefix");
     println!(".globl main");
     println!("main:");
-    println!("  mov rax, {}", str_to_u(&mut c));
 
-    loop {
-        match c.next() {
-            Some('+')=> println!("  add rax, {}", str_to_u(&mut c)),
-            Some('-') => println!("  sub rax, {}", str_to_u(&mut c)),
-            Some(other) => panic!("予期しない文字です: {}", other),
-            None => break,
+    // 最初は数値でなければならない
+    println!("  mov rax, {}", expect_number(&mut token_iter).unwrap());
+
+    while let Some(token) = token_iter.next() {
+        match token {
+            Token::Minus => println!("  sub rax, {}", expect_number(&mut token_iter).unwrap()),
+            Token::Plus => println!("  add rax, {}", expect_number(&mut token_iter).unwrap()),
+            _ => panic!("予期しないトークンです: {:?}", token),
         }
     }
 
     println!("  ret");
-
     Ok(())
 }
